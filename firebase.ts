0c5e00c8bd8fc8eb7936f3e3a24c5f8ff4933f94
tesla-user <tesla@example.com>
2026-07-10 21:29:20 +0100

    commit all

--- firebase.ts
+++ firebase.ts
@@ -1,19 +1,21 @@
 import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || 'AIzaSyCTZ7Drg7DvWxKe5n7VqzagAIqhs4SY38w',
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || 'tesl-f61ed.firebaseapp.com',
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || 'tesl-f61ed',
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || 'tesl-f61ed.firebasestorage.app',
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || '846003377526',
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || '1:846003377526:web:3ec730861acb69c5469657',
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || 'G-ER162HY8ZN',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || 'AIzaSyByJ9G3R5R8V_CYTNXNNuV6ZWQo1cPDt_A',
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || 'tesla-3863b.firebaseapp.com',
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || 'tesla-3863b',
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || 'tesla-3863b.firebasestorage.app',
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || '684384171989',
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || '1:684384171989:web:f51602b476f675d6f1af5c',
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || 'G-YTKGPSYB70',
 }
 
 const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 
 export const auth = getAuth(app)
 export const db = getFirestore(app)
@@ -21,6 +23,7 @@ export const storage = getStorage(app)
 export const googleProvider = new GoogleAuthProvider()
 
 export default app
+export { analytics }
 
 
 
